2965e1faba6ace374b83d3ace8840479617a0868
kurocat471 <jonathanpc471@gmail.com>
2023-08-12 18:23:46 -0500

    Initial commit

---
+++ src/main/java/net/kuro/recrafted/Recrafted.java
@@ -0,0 +1,16 @@
+package net.kuro.recrafted;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Recrafted implements ModInitializer {
+	public static final String MOD_ID = "mc-recrafted";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/kuro/recrafted/RecraftedClient.java
@@ -0,0 +1,10 @@
+package net.kuro.recrafted;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecraftedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
