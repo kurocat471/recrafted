3cb338da8bdb50877bc6f6349df64a2dc4dc6634
kurocat471 <jonathanpc471@gmail.com>
2024-01-13 18:54:23 -0600

    added copper drops and minability

--- src/main/java/net/kuro/recrafted/datagen/ModBlockTagProvider.java
+++ src/main/java/net/kuro/recrafted/datagen/ModBlockTagProvider.java
@@ -2,7 +2,9 @@ package net.kuro.recrafted.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.kuro.recrafted.structure.block.ModBlocks;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +15,171 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+            .add(
+                ModBlocks.COPPER_BLOCK,
+                ModBlocks.EXPOSED_COPPER_BLOCK,
+                ModBlocks.WEATHERED_COPPER_BLOCK,
+                ModBlocks.OXIDIZED_COPPER_BLOCK,
+                ModBlocks.WAXED_COPPER_BLOCK,
+                ModBlocks.WAXED_EXPOSED_COPPER_BLOCK,
+                ModBlocks.WAXED_WEATHERED_COPPER_BLOCK,
+                ModBlocks.WAXED_OXIDIZED_COPPER_BLOCK,
 
+                ModBlocks.COPPER_GRATE,
+                ModBlocks.EXPOSED_COPPER_GRATE,
+                ModBlocks.WEATHERED_COPPER_GRATE,
+                ModBlocks.OXIDIZED_COPPER_GRATE,
+                ModBlocks.WAXED_COPPER_GRATE,
+                ModBlocks.WAXED_EXPOSED_COPPER_GRATE,
+                ModBlocks.WAXED_WEATHERED_COPPER_GRATE,
+                ModBlocks.WAXED_OXIDIZED_COPPER_GRATE,
+
+                ModBlocks.CHISELED_COPPER,
+                ModBlocks.EXPOSED_CHISELED_COPPER,
+                ModBlocks.WEATHERED_CHISELED_COPPER,
+                ModBlocks.OXIDIZED_CHISELED_COPPER,
+                ModBlocks.WAXED_CHISELED_COPPER,
+                ModBlocks.WAXED_EXPOSED_CHISELED_COPPER,
+                ModBlocks.WAXED_WEATHERED_CHISELED_COPPER,
+                ModBlocks.WAXED_OXIDIZED_CHISELED_COPPER,
+
+                ModBlocks.CUT_COPPER,
+                ModBlocks.EXPOSED_CUT_COPPER,
+                ModBlocks.WEATHERED_CUT_COPPER,
+                ModBlocks.OXIDIZED_CUT_COPPER,
+                ModBlocks.WAXED_CUT_COPPER,
+                ModBlocks.WAXED_EXPOSED_CUT_COPPER,
+                ModBlocks.WAXED_WEATHERED_CUT_COPPER,
+                ModBlocks.WAXED_OXIDIZED_CUT_COPPER,
+
+                ModBlocks.CUT_COPPER_STAIRS,
+                ModBlocks.EXPOSED_CUT_COPPER_STAIRS,
+                ModBlocks.WEATHERED_CUT_COPPER_STAIRS,
+                ModBlocks.OXIDIZED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_EXPOSED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_WEATHERED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_OXIDIZED_CUT_COPPER_STAIRS,
+
+                ModBlocks.CUT_COPPER_SLAB,
+                ModBlocks.EXPOSED_CUT_COPPER_SLAB,
+                ModBlocks.WEATHERED_CUT_COPPER_SLAB,
+                ModBlocks.OXIDIZED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_EXPOSED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_WEATHERED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_OXIDIZED_CUT_COPPER_SLAB,
+
+                ModBlocks.COPPER_DOOR,
+                ModBlocks.EXPOSED_COPPER_DOOR,
+                ModBlocks.WEATHERED_COPPER_DOOR,
+                ModBlocks.OXIDIZED_COPPER_DOOR,
+                ModBlocks.WAXED_COPPER_DOOR,
+                ModBlocks.WAXED_EXPOSED_COPPER_DOOR,
+                ModBlocks.WAXED_WEATHERED_COPPER_DOOR,
+                ModBlocks.WAXED_OXIDIZED_COPPER_DOOR,
+
+                ModBlocks.COPPER_TRAPDOOR,
+                ModBlocks.EXPOSED_COPPER_TRAPDOOR,
+                ModBlocks.WEATHERED_COPPER_TRAPDOOR,
+                ModBlocks.OXIDIZED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_EXPOSED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_WEATHERED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_OXIDIZED_COPPER_TRAPDOOR,
+
+                ModBlocks.COPPER_BULB,
+                ModBlocks.EXPOSED_COPPER_BULB,
+                ModBlocks.WEATHERED_COPPER_BULB,
+                ModBlocks.OXIDIZED_COPPER_BULB,
+                ModBlocks.WAXED_COPPER_BULB,
+                ModBlocks.WAXED_EXPOSED_COPPER_BULB,
+                ModBlocks.WAXED_WEATHERED_COPPER_BULB,
+                ModBlocks.WAXED_OXIDIZED_COPPER_BULB
+            );
+        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
+            .add(
+                ModBlocks.COPPER_BLOCK,
+                ModBlocks.EXPOSED_COPPER_BLOCK,
+                ModBlocks.WEATHERED_COPPER_BLOCK,
+                ModBlocks.OXIDIZED_COPPER_BLOCK,
+                ModBlocks.WAXED_COPPER_BLOCK,
+                ModBlocks.WAXED_EXPOSED_COPPER_BLOCK,
+                ModBlocks.WAXED_WEATHERED_COPPER_BLOCK,
+                ModBlocks.WAXED_OXIDIZED_COPPER_BLOCK,
+
+                ModBlocks.COPPER_GRATE,
+                ModBlocks.EXPOSED_COPPER_GRATE,
+                ModBlocks.WEATHERED_COPPER_GRATE,
+                ModBlocks.OXIDIZED_COPPER_GRATE,
+                ModBlocks.WAXED_COPPER_GRATE,
+                ModBlocks.WAXED_EXPOSED_COPPER_GRATE,
+                ModBlocks.WAXED_WEATHERED_COPPER_GRATE,
+                ModBlocks.WAXED_OXIDIZED_COPPER_GRATE,
+
+                ModBlocks.CHISELED_COPPER,
+                ModBlocks.EXPOSED_CHISELED_COPPER,
+                ModBlocks.WEATHERED_CHISELED_COPPER,
+                ModBlocks.OXIDIZED_CHISELED_COPPER,
+                ModBlocks.WAXED_CHISELED_COPPER,
+                ModBlocks.WAXED_EXPOSED_CHISELED_COPPER,
+                ModBlocks.WAXED_WEATHERED_CHISELED_COPPER,
+                ModBlocks.WAXED_OXIDIZED_CHISELED_COPPER,
+
+                ModBlocks.CUT_COPPER,
+                ModBlocks.EXPOSED_CUT_COPPER,
+                ModBlocks.WEATHERED_CUT_COPPER,
+                ModBlocks.OXIDIZED_CUT_COPPER,
+                ModBlocks.WAXED_CUT_COPPER,
+                ModBlocks.WAXED_EXPOSED_CUT_COPPER,
+                ModBlocks.WAXED_WEATHERED_CUT_COPPER,
+                ModBlocks.WAXED_OXIDIZED_CUT_COPPER,
+
+                ModBlocks.CUT_COPPER_STAIRS,
+                ModBlocks.EXPOSED_CUT_COPPER_STAIRS,
+                ModBlocks.WEATHERED_CUT_COPPER_STAIRS,
+                ModBlocks.OXIDIZED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_EXPOSED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_WEATHERED_CUT_COPPER_STAIRS,
+                ModBlocks.WAXED_OXIDIZED_CUT_COPPER_STAIRS,
+
+                ModBlocks.CUT_COPPER_SLAB,
+                ModBlocks.EXPOSED_CUT_COPPER_SLAB,
+                ModBlocks.WEATHERED_CUT_COPPER_SLAB,
+                ModBlocks.OXIDIZED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_EXPOSED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_WEATHERED_CUT_COPPER_SLAB,
+                ModBlocks.WAXED_OXIDIZED_CUT_COPPER_SLAB,
+
+                ModBlocks.COPPER_DOOR,
+                ModBlocks.EXPOSED_COPPER_DOOR,
+                ModBlocks.WEATHERED_COPPER_DOOR,
+                ModBlocks.OXIDIZED_COPPER_DOOR,
+                ModBlocks.WAXED_COPPER_DOOR,
+                ModBlocks.WAXED_EXPOSED_COPPER_DOOR,
+                ModBlocks.WAXED_WEATHERED_COPPER_DOOR,
+                ModBlocks.WAXED_OXIDIZED_COPPER_DOOR,
+
+                ModBlocks.COPPER_TRAPDOOR,
+                ModBlocks.EXPOSED_COPPER_TRAPDOOR,
+                ModBlocks.WEATHERED_COPPER_TRAPDOOR,
+                ModBlocks.OXIDIZED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_EXPOSED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_WEATHERED_COPPER_TRAPDOOR,
+                ModBlocks.WAXED_OXIDIZED_COPPER_TRAPDOOR,
+
+                ModBlocks.COPPER_BULB,
+                ModBlocks.EXPOSED_COPPER_BULB,
+                ModBlocks.WEATHERED_COPPER_BULB,
+                ModBlocks.OXIDIZED_COPPER_BULB,
+                ModBlocks.WAXED_COPPER_BULB,
+                ModBlocks.WAXED_EXPOSED_COPPER_BULB,
+                ModBlocks.WAXED_WEATHERED_COPPER_BULB,
+                ModBlocks.WAXED_OXIDIZED_COPPER_BULB
+            );
     }
 }
--- src/main/java/net/kuro/recrafted/datagen/ModModelProvider.java
+++ src/main/java/net/kuro/recrafted/datagen/ModModelProvider.java
@@ -13,22 +13,7 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerDoor(ModBlocks.COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.EXPOSED_COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.WEATHERED_COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.OXIDIZED_COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.WAXED_COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.WAXED_EXPOSED_COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.WAXED_WEATHERED_COPPER_DOOR);
-        blockStateModelGenerator.registerDoor(ModBlocks.WAXED_OXIDIZED_COPPER_DOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.EXPOSED_COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.WEATHERED_COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.OXIDIZED_COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.WAXED_COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.WAXED_EXPOSED_COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.WAXED_WEATHERED_COPPER_TRAPDOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.WAXED_OXIDIZED_COPPER_TRAPDOOR);
+
     }
 
     @Override
