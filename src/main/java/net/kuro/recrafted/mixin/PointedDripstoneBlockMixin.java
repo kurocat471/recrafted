82c280824138e74a7d9617541526d0c197ce7c88
kurocat471 <jonathanpc471@gmail.com>
2024-01-14 21:28:51 -0600

    mixin cleanup

--- src/main/java/net/kuro/recrafted/mixin/PointedDripstoneBlockMixin.java
+++ src/main/java/net/kuro/recrafted/mixin/PointedDripstoneBlockMixin.java
@@ -1,5 +1,6 @@
 package net.kuro.recrafted.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.kuro.recrafted.structure.block.custom.barrel.AbstractBarrelBlock;
 import net.minecraft.block.AbstractCauldronBlock;
 import net.minecraft.block.BlockState;
@@ -11,9 +12,11 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
@@ -23,28 +26,8 @@ import java.util.function.Predicate;
 @Mixin(PointedDripstoneBlock.class)
 public abstract class PointedDripstoneBlockMixin {
 
-    @Invoker("canDripThrough")
-    public static boolean invokeCanDripThrough(BlockView world, BlockPos pos, BlockState state) {
-        throw new AssertionError();
-    }
-
-    @Invoker("searchInDirection")
-    public static Optional<BlockPos> invokeSearchInDirection(WorldAccess world, BlockPos pos, Direction.AxisDirection direction, BiPredicate<BlockPos, BlockState> continuePredicate, Predicate<BlockState> stopPredicate, int range) {
-        throw new AssertionError();
-    }
-
-    @Inject(method = "getCauldronPos", at = @At("HEAD"), cancellable = true)
-    private static void getCauldronOrBarrelPos(World world, BlockPos pos, Fluid fluid, CallbackInfoReturnable<BlockPos> cir) {
-        Predicate<BlockState> predicate = state -> {
-            if (state.getBlock() instanceof AbstractCauldronBlock) {
-                return state.getBlock() instanceof AbstractCauldronBlock && ((AbstractCauldronBlockInvoker) state.getBlock()).invokeCanBeFilledByDripstone(fluid);
-            } else if (state.getBlock() instanceof AbstractBarrelBlock) {
-                return state.getBlock() instanceof AbstractBarrelBlock && ((AbstractBarrelBlock) state.getBlock()).canBeFilledByDripstone(fluid);
-            }
-            return false;
-        };
-        BiPredicate<BlockPos, BlockState> biPredicate = (posx, state) -> invokeCanDripThrough(world, posx, state);
-        BlockPos result = invokeSearchInDirection(world, pos, Direction.DOWN.getDirection(), biPredicate, predicate, 11).orElse(null);
-        cir.setReturnValue(result);
+    @ModifyVariable(method = "getCauldronPos", at = @At("STORE"))
+    private static Predicate<BlockState> getCauldronOrBarrelPos(Predicate<BlockState> predicate, World world, BlockPos pos, Fluid fluid) {
+        return predicate.or(state -> state.getBlock() instanceof AbstractBarrelBlock && ((AbstractBarrelBlock) state.getBlock()).canBeFilledByDripstone(fluid));
     }
 }
